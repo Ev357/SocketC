e5d1f055fa822623cbbecd0d8337eec2b70449da
Ev357 <evest.tech@gmail.com>
2023-02-04 18:12:58 +0000

    updated

--- client.py
+++ client.py
@@ -1,18 +1,14 @@
 import socket
-import ssl
 
 HOST = 'socketc.onrender.com'
-PORT = 443
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+PORT = 80
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    with context.wrap_socket(s, server_hostname=HOST) as ssock:
-        while True:
-            message = input('Enter your message: ')
-            ssock.sendall(message.encode())
-            data = ssock.recv(1024)
-            print('Received', repr(data.decode()))
-            if message == 'close':
-                break
+    while True:
+        message = input('Enter your message: ')
+        s.sendall(message.encode())
+        data = s.recv(1024)
+        print('Received', repr(data.decode()))
+        if message == 'close':
+            break
--- server.py
+++ server.py
@@ -1,21 +1,15 @@
 import socket
-import ssl
 
 HOST = '0.0.0.0'
 PORT = 10000
-
-context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
-context.load_cert_chain(certfile='ssl/cert.pem', keyfile='ssl/key.pem')
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     while True:
         conn, addr = s.accept()
-        print('Connected by', addr)
-        with context.wrap_socket(conn, server_side=True) as ssock:
-            while True:
-                data = ssock.recv(1024)
-                if not data:
-                    break
-                ssock.sendall(data[::-1])
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data[::-1])
